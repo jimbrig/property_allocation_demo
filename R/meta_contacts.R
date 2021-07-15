02705bdf3962351d3412f71f0898252cca8b2b80
Jimmy Briggs <jimbrig1993@outlook.com>
2021-07-15 13:53:40 -0400

    update contacts

--- R/meta_contacts.R
+++ R/meta_contacts.R
@@ -5,18 +5,6 @@
 contacts <- function() {
   c(contact_item("Jimmy Briggs",
                  "Consultant",
-                 "404-239-6402",
-                 "jimmy.briggs@oliverwyman.com"),
-    contact_item("Jack Reardon",
-                 "Consultant",
-                 "415-743-7821",
-                 "jack.reardon@oliverwyman.com"),
-    contact_item("Adam Lewis",
-                 "Principal",
-                 "404-239-6428",
-                 "adam.lewis@oliverwyman.com"),
-    contact_item("Brian Settle",
-                 "Principal",
-                 "404-239-6407",
-                 "brian.settle@oliverwyman.com"))
+                 "678-491-4856",
+                 "jimbrig1993@outlook.com"))
 }
