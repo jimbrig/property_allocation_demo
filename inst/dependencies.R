4f4230e9ea3fc283f85ae823ea15310e0f4c516c
Jimmy Briggs <jimbrig1993@outlook.com>
2021-07-15 13:53:52 -0400

    deps

--- inst/dependencies.R
+++ inst/dependencies.R
@@ -1,10 +1,40 @@
 # No Remotes ----
 # Attachments ----
-to_install <- c("cli", "dplyr", "DT", "formattable", "fs", "highcharter", "htmltools", "htmlwidgets", "janitor", "kableExtra", "knitr", "lubridate", "magrittr", "matchmaker", "purrr", "rhandsontable", "rintrojs", "rlang", "scales", "shiny", "shinycustomloader", "shinydashboard", "shinyjs", "shinyWidgets", "stringr", "summarytools", "tibble", "tidyr", "tidyselect")
-  for (i in to_install) {
-    message(paste("looking for ", i))
-    if (!requireNamespace(i)) {
-      message(paste("     installing", i))
-      require(i)
-    }
+to_install <-
+  c("cli",
+    "dplyr",
+    "DT",
+    "formattable",
+    "fs",
+    "highcharter",
+    "htmltools",
+    "htmlwidgets",
+    "janitor",
+    "kableExtra",
+    "knitr",
+    "lubridate",
+    "magrittr",
+    "matchmaker",
+    "purrr",
+    "rhandsontable",
+    "rintrojs",
+    "rlang",
+    "scales",
+    "shiny",
+    "shinycustomloader",
+    "shinydashboard",
+    "shinyjs",
+    "shinyWidgets",
+    "stringr",
+    "summarytools",
+    "tibble",
+    "tidyr",
+    "tidyselect")
+
+for (i in to_install) {
+  message(paste("looking for ", i))
+  if (!requireNamespace(i)) {
+    message(paste("     installing", i))
+    require(i)
   }
+}
